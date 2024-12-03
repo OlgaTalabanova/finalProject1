9b60bc7efeee73217314a995f473335fc4f7a5e7
Maxim <maxtalab@gmail.com>
2024-12-03 18:56:23 +0300

    2

--- venv/request_sender.py
+++ venv/request_sender.py
@@ -7,9 +7,7 @@ def create_order():
                               json = data.order_body)
     return response.json()['track']
 
-def get_order_details(trackNumber):
-    response = requests.get(configuration.ORDER_URL + "/track", params= {"t":trackNumber})
-    return response.json ()
+
 
 
 
