c0a73e9b1b28508983114abc738827b77b7a8f32
Maxim <maxtalab@gmail.com>
2024-12-03 19:11:22 +0300

    2

--- configuration.py
+++ configuration.py
@@ -1,2 +1,2 @@
-SERVICE_URL = "https://5430e932-9d99-460a-be67-cae6d07d76fb.serverhub.praktikum-services.ru"
+SERVICE_URL = "https://ac05f27e-99a9-4fa4-9ffd-917f215a66c2.serverhub.praktikum-services.ru"
 ORDER_URL = SERVICE_URL + "/api/v1/orders"
--- request_sender.py
+++ request_sender.py
@@ -5,11 +5,10 @@ import data
 def create_order():
     response = requests.post (configuration.ORDER_URL,
                               json = data.order_body)
-    assert response.status_code == 201
     return response.json()['track']
 
-def get_order_details(trackNumber):
-    response = requests.get(configuration.ORDER_URL + "/track", params= {"t":trackNumber})
-    assert response.status_code == 200
-    return response.json ()
+
+
+
+
 
